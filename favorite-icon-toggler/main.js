5053185027d4cba2146acb70e2ec9ba4a9712829
EvesLastDescendant <clydewitoutabonnie@mail.com>
2026-09-17 10:38:22 +0100

    new branch and challenges completed 16-09-2026 -- 17-09-2026

---
+++ favorite-icon-toggler/main.js
@@ -0,0 +1,8 @@
+const favoriteIcons = document.querySelectorAll('.favorite-icon');
+
+favoriteIcons.forEach(icon => {
+    icon.addEventListener('click', function() {
+        this.classList.toggle('filled');
+        this.innerHTML = this.classList.contains('filled') ? '&#10084;' : '&#9825;';
+    });
+});
